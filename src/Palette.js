4e5f6ed6051f7c5a16b271fc30fef037ac8b25ed
Ujjval <ujjvalkumaria@gmail.com>
2019-09-27 12:57:12 -0700

    added palette skeleton

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Palette = () => {
+    return (
+        <div className="palette">
+            {/* navbar goes here */}
+            <div className="palette-colors">
+                {/* bunch of color boxes */}
+            </div>
+            {/* footer goes here */}
+        </div>
+    )
+}
+
+export default Palette

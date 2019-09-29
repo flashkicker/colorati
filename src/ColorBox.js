6652f0bbb2eb8e9d381f95b4c19642ca0aa0d436
Ujjval <ujjvalkumaria@gmail.com>
2019-09-29 15:56:04 -0700

    added colorHelper to generate colors

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,12 +3,33 @@ import { CopyToClipboard } from "react-copy-to-clipboard"
 import "./color-box.css"
 
 class ColorBox extends Component {
+	state = {
+		copied: false
+	}
+
+	changeCopyState = () => {
+		this.setState({ copied: true }, () => {
+			setTimeout(() => {
+				this.setState({ copied: false })
+			}, 1500)
+		})
+	}
+
 	render() {
 		const { name, background } = this.props
+		const { copied } = this.state
 
 		return (
-			<CopyToClipboard text={background}>
+			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div className="color-box" style={{ background }}>
+					<div
+						className={`copy-overlay ${copied && "show"}`}
+						style={{ background }}
+					/>
+					<div className={`copy-msg ${copied && "show"}`}>
+						<h1>COPIED!</h1>
+						<p>{background}</p>
+					</div>
 					<div className="copy-container">
 						<div className="box-content">
 							<span>{name}</span>

202f5b3bc4667d84ef6614b1424684ad22d160f7
Ujjval <ujjvalkumaria@gmail.com>
2019-10-01 19:02:18 -0700

    added format change feature

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react"
 import Slider from "rc-slider"
-
+import { Select, MenuItem, Snackbar, IconButton } from "@material-ui/core"
+import CloseIcon from "@material-ui/icons/Close"
 import "rc-slider/assets/index.css"
 import "./navbar.css"
 
 class Navbar extends Component {
+	state = {
+		format: "hex",
+		open: false
+	}
+
+	handleFormatChange = event => {
+		this.setState({ format: event.target.value, open: true })
+		this.props.changeFormat(event.target.value)
+	}
+
+	closeSnackbar = () => {
+		this.setState({ open: false })
+	}
+
 	render() {
 		const { level, changeLevel } = this.props
+		const { format } = this.state
 
 		return (
 			<header className="navbar">
@@ -25,6 +41,33 @@ class Navbar extends Component {
 						/>
 					</div>
 				</div>
+				<div className="select-container">
+					<Select value={format} onChange={this.handleFormatChange} className="select-format">
+						<MenuItem value="hex">HEX - ex. #ffffff</MenuItem>
+						<MenuItem value="rgb">RGB - ex. rgb(255, 255, 255)</MenuItem>
+						<MenuItem value="rgba">
+							RGBA - ex. rgba(255, 255, 255, 1.0)
+						</MenuItem>
+					</Select>
+				</div>
+				<Snackbar
+					open={this.state.open}
+					autoHideDuration={3000}
+					message={<span id="message-id">Format changed to {format}!</span>}
+					ContentProps={{ "aria-describedby": "message-id" }}
+					anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+					onClose={this.closeSnackbar}
+					action={[
+						<IconButton
+							color="inherit"
+							key="close"
+							aria-label="close"
+							onClick={this.closeSnackbar}
+						>
+							<CloseIcon />
+						</IconButton>
+					]}
+				/>
 			</header>
 		)
 	}
